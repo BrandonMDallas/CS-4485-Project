f4814590555686b001aa901b97358cefb258c9df
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-27 12:54:08 -0600

    Initializing backend

---
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/WidgetappApplication.java
@@ -0,0 +1,13 @@
+package com.cs4485.group2.widgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetappApplication.class, args);
+	}
+
+}
---
+++ backend/widgetapp/src/test/java/com/cs4485/group2/widgetapp/WidgetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs4485.group2.widgetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidgetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
