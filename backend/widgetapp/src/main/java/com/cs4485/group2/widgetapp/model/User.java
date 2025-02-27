f4672b093912312adacaab43fabc38641c19fa94
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-27 15:17:28 -0600

    Forgot an annotation

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/User.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/User.java
@@ -2,6 +2,7 @@ package com.cs4485.group2.widgetapp.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "users")
 public class User {
+    @Id
     private long id;
     private String username;
 
