f2ea3438ea53fac74aee25aea521ddcda19c4429
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-28 16:42:56 -0600

    Finished Widget implementation

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
@@ -1,11 +1,29 @@
 package com.cs4485.group2.widgetapp.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "widgets")
 public class Widget {
+    @Id
+    private long id;
+    @Column(name = "user_id")
+    private long userId;
+
+    private String title;
+
+    private String description;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
