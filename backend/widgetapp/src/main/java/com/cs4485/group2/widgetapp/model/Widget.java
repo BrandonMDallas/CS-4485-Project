049cf562589b6ec45b31d3fc096619404327f7d8
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-02-28 16:40:11 -0600

    Setting up Widget class

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
@@ -1,4 +1,11 @@
 package com.cs4485.group2.widgetapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "widgets")
 public class Widget {
 }
