c169c5125c8c4773655617b48274bf03ba53524b
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-06 17:49:54 -0600

    Finished WidgetMapper impl

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/mapper/WidgetMapper.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/mapper/WidgetMapper.java
@@ -1,4 +1,28 @@
 package com.cs4485.group2.widgetapp.mapper;
 
+import com.cs4485.group2.widgetapp.dto.WidgetDto;
+import com.cs4485.group2.widgetapp.model.Widget;
+
 public class WidgetMapper {
+    public static WidgetDto mapToWidgetDto(Widget widget) {
+        return WidgetDto.builder()
+                .id(widget.getId())
+                .userId(widget.getUserId())
+                .title(widget.getTitle())
+                .description(widget.getDescription())
+                .createdAt(widget.getCreatedAt())
+                .updatedAt(widget.getUpdatedAt())
+                .build();
+    }
+
+    public static Widget mapToWidget(WidgetDto widgetDto) {
+        return new Widget(
+                widgetDto.getId(),
+                widgetDto.getUserId(),
+                widgetDto.getTitle(),
+                widgetDto.getDescription(),
+                widgetDto.getCreatedAt(),
+                widgetDto.getUpdatedAt()
+        );
+    }
 }
--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/model/Widget.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "widgets")
 public class Widget {
