9751311a644ce0143b3d6f45479d650695bfa1ab
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-06 17:45:29 -0600

    Finished WidgetRepository interface

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/repository/WidgetRepository.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/repository/WidgetRepository.java
@@ -1,4 +1,10 @@
 package com.cs4485.group2.widgetapp.repository;
 
-public interface WidgetRepository {
+import com.cs4485.group2.widgetapp.model.Widget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WidgetRepository extends JpaRepository<Widget, Long> {
+    Optional<Widget> findByUserId(Long userId);
 }
