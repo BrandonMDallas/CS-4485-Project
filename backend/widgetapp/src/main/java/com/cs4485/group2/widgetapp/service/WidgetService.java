588f6cccdc45f5760025b12c6cc27ea69980a055
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-06 17:51:42 -0600

    Finished WidgetService interface impl

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/service/WidgetService.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/service/WidgetService.java
@@ -1,4 +1,17 @@
 package com.cs4485.group2.widgetapp.service;
 
+import com.cs4485.group2.widgetapp.dto.WidgetDto;
+
+import java.util.List;
+
 public interface WidgetService {
+    WidgetDto createWidget(WidgetDto widgetDto);
+
+    WidgetDto getWidgetById(Long widgetId);
+
+    List<WidgetDto> getAllWidgets();
+
+    WidgetDto updateWidget(Long widgetId, WidgetDto updatedWidget);
+
+    void deleteWidget(Long widget);
 }
