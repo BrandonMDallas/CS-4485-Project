a2097dd333871c9c7c8687da72d359781aa33958
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-06 17:44:03 -0600

    Finished WidgetDto impl

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/dto/WidgetDto.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/dto/WidgetDto.java
@@ -1,4 +1,17 @@
 package com.cs4485.group2.widgetapp.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class WidgetDto {
+    private long id;
+    private long userId;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
