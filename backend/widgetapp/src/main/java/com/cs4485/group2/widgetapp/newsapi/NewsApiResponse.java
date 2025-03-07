3f6974edfa24a06dde1159a447ef0fb47770cbe8
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-06 18:22:08 -0600

    Impl a dotenv initializer

---
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/newsapi/NewsApiResponse.java
@@ -0,0 +1,4 @@
+package com.cs4485.group2.widgetapp.newsapi;
+
+public class NewsApiResponse {
+}
---
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/newsapi/config/DotenvInitializer.java
@@ -0,0 +1,25 @@
+package com.cs4485.group2.widgetapp.newsapi.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing() // Optional: ignore if the .env file is not found
+                .load();
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("NEWSAPI_API_KEY", dotenv.get("NEWSAPI_API_KEY"));
+
+        MapPropertySource propertySource = new MapPropertySource("dotenvProperties", map);
+        applicationContext.getEnvironment().getPropertySources().addFirst(propertySource);
+    }
+}
