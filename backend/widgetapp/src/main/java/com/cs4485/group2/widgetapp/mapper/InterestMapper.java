8f65ea78825b8c5196d459cba4ab608dad2d7ac6
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-14 08:23:42 -0500

    Adding the interest mapper

--- backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/mapper/InterestMapper.java
+++ backend/widgetapp/src/main/java/com/cs4485/group2/widgetapp/mapper/InterestMapper.java
@@ -1,4 +1,26 @@
 package com.cs4485.group2.widgetapp.mapper;
 
+import com.cs4485.group2.widgetapp.dto.InterestDto;
+import com.cs4485.group2.widgetapp.model.Interest;
+
+
 public class InterestMapper {
+    public static InterestDto mapToInterestDto(Interest interest){
+        return InterestDto.builder()
+                .id(interest.getId())
+                .name(interest.getName())
+                .description(interest.getDescription())
+                .createdAt(interest.getCreatedAt())
+                .updatedAt(interest.getUpdatedAt())
+                .build();
+    }
+    public static Interest mapToInterest(InterestDto interestDto) {
+        return new Interest(
+                interestDto.getId(),
+                interestDto.getName(),
+                interestDto.getDescription(),
+                interestDto.getCreatedAt(),
+                interestDto.getUpdatedAt()
+        );
+    }
 }
