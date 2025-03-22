13b11ec6a417101ae028e572ce48c21f2162978a
Aidan Turgut <82731582+AidanTurgut@users.noreply.github.com>
2025-03-22 15:14:52 -0500

    Create whatStock.jsx

---
+++ react/whatStock.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ReactDOM from 'react-dom/client';
+import './App.css'
+import 'https://cdn.jsdelivr.net/npm/chart.js'
+import Modal from 'react-bootstrap/Modal'
+import {FaSearch} from "react-icons/fa"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import MainMenu from './MainMenu.jsx'
+import Stocks from './StocksPage.jsx'
+import {createElement} from 'react';
+import {Chart as ChartJS, LinearScale, LineElement, CategoryScale, PointElement} from 'chart.js'
+import {Line} from 'react-chartjs-2'
+import Axios from 'axios';
+import Accordion from 'react-bootstrap/Accordion';
+
+
+function aiOne() {
+  return(
+    <>
+   <h1>What best describes your financial desires?</h1>
+<Form>
+<Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+</Form>
+   </>
+  )
+}
+export default aiOne;
