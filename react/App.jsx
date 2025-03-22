8fd41b7714ffc1a178590d76a1c64d7f202e8adb
Aidan Turgut <82731582+AidanTurgut@users.noreply.github.com>
2025-03-22 15:11:40 -0500

    Create App.jsx

---
+++ react/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ReactDOM from 'react-dom/client';
+import './App.css'
+import 'https://cdn.jsdelivr.net/npm/chart.js'
+import Modal from 'react-bootstrap/Modal'
+import {FaSearch} from "react-icons/fa"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Form from 'react-bootstrap/Form'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import MainMenu from './MainMenu.jsx'
+import Stocks from './StocksPage.jsx'
+import {createElement} from 'react';
+import MusicHub from './musicHub.jsx'
+import AiFinance from './aiFinance.jsx'
+import {Chart as ChartJS, LinearScale, LineElement, CategoryScale, PointElement} from 'chart.js'
+import {Line} from 'react-chartjs-2'
+import Axios from 'axios';
+import SportsHub from './SportsHub.jsx'
+import WhatStock from './whatStock.jsx'
+import ImprovementStocks from './improveStocks.jsx'
+import ProfilePage from './profilePage.jsx'
+import StocksSetting from './stocksSettings.jsx'
+function App() {
+  return(
+    <Router>
+      <Routes>
+      <Route path="/" element={<MainMenu/>}/>
+            <Route path="/stocks" element={<Stocks/>}/>
+            <Route path="/musicHub" element={<MusicHub/>}/>
+            <Route path="/aifinance" element={<AiFinance/>}/>
+            <Route path="/sportsHub" element={<SportsHub/>}/>
+            <Route path="/whatStock" element={<WhatStock/>}/>
+            <Route path="/improveStocks" element={<ImprovementStocks/>}/>
+            <Route path="/profilePage" element={<ProfilePage/>}/>
+            <Route path="/stocksSetting" element={<StocksSetting/>}/>
+          </Routes>
+          <Link to="/">Go to mainMenu</Link>
+    </Router>
+  )
+}
+export default App;
