423d1916fdb3acbb1fa2d36b844055f3870db6f1
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-03-26 21:07:30 -0500

    Added the App.jsx

---
+++ react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginSignup from './components/loginSignup/LoginSignup'
+const App = () => {
+  return (
+    <div>App
+        <LoginSignup/>
+    </div>
+  )
+}
+
+export default App
