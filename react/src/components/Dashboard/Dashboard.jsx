35d8337e00dfcdd2f34fc9883c84135d07fd6481
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-04-12 19:41:28 -0500

    Small changes

--- react/src/components/Dashboard/Dashboard.jsx
+++ react/src/components/Dashboard/Dashboard.jsx
@@ -15,6 +15,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Carousel from "react-bootstrap/Carousel";
 import Accordion from "react-bootstrap/Accordion";
 import { Link } from "react-router";
+import MainTab from "./MainTab";
 function Dashboard() {
   /*let navigate = useNavigate();
 
@@ -37,7 +38,7 @@ function Dashboard() {
   const [newsHeader, setNewsHeader] = useState("");
   const [newsImage, setNewsImage] = useState("");
   useEffect(() => {
-    getNews();
+    //getNews();
   }, []);
   async function getNews() {
     await Axios.get(
@@ -89,80 +90,7 @@ function Dashboard() {
         className="mb-3"
       >
         <Tab eventKey="mainPage" title="Main">
-          <h2 style={{ textAlign: "center", fontFamily: "sans-serif" }}>
-            What would you like to do today?
-          </h2>
-          <div style={{ display: "inline-flex" }}>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://w0.peakpx.com/wallpaper/286/926/HD-wallpaper-lebron-james-los-angeles-lakers-nba-famous-basketball-players-american-basketball-player-art-purple-stone-background-usa-basketball.jpg"
-                height="45%"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Sports</h5>
-                <p class="card-text">
-                  Catch up with the latest news in sports, players, and teams!
-                </p>
-                <Link to="/sportsHub">
-                  <a href="#" class="btn btn-primary">
-                    Visit
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://storage.googleapis.com/research-production/1/2024/06/RS064-Socially-Motivated-Music-Recommendation_1_Without-Logo.png"
-                height="45%"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Music</h5>
-                <p class="card-text">
-                  Listen to the latest and most popular songs currently out
-                  there!
-                </p>
-                <Link to="/musicHub">
-                  <a
-                    href="#"
-                    style={{ backgroundColor: "purple", borderColor: "purple" }}
-                    class="btn btn-primary"
-                  >
-                    Visit
-                  </a>
-                </Link>
-              </div>
-            </div>
-            <div class="card" style={{ width: "18rem" }}>
-              <img
-                src="https://www.analyticssteps.com/backend/media/thumbnail/8517708/4201790_1646043215_What%20are%20StocksArtboard%201%20(1).jpg"
-                height="45%"
-                class="card-img-top"
-                alt="..."
-              />
-              <div class="card-body">
-                <h5 class="card-title">Stocks</h5>
-                <p class="card-text">
-                  View and keep track of stock data from various companies!
-                </p>
-                <Link to="/stocks">
-                  <a
-                    style={{
-                      backgroundColor: "green",
-                      borderColor: "green",
-                      color: "white",
-                    }}
-                    class="btn btn-primary"
-                  >
-                    Visit
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
+          <MainTab />
         </Tab>
         <Tab eventKey="profile" title="Your Profile">
           <Accordion defaultActiveKey="0">
