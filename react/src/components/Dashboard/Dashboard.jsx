9d30c9362d350c46adcf640a466c83dedb2825f6
Aidan Turgut <aidantur@gmail.com>
2025-04-25 10:42:37 -0500

    aidan fixed the slide show part of the dashboard page

--- react/src/components/Dashboard/Dashboard.jsx
+++ react/src/components/Dashboard/Dashboard.jsx
@@ -8,22 +8,61 @@ import SettingsTab from "./SettingsTab";
 import styles from "./Dashboard.module.css";
 import Carousel from 'react-bootstrap/Carousel';
 import { useState, useEffect } from 'react'
+import { parse } from "postcss";
+import axios from 'axios';
+import Axios from 'axios';
 
 const Dashboard = () => {
-  const [newsHeader, setNewsHeader]=useState("")
-  const [newsImage, setNewsImage]=useState("")
+  const [newsMusicHeader, setNewsMusicHeader]=useState("")
+  const [newsMusicImage, setNewsMusicImage]=useState("")
+  const [newsSportsHeader, setNewsSportsHeader]=useState("")
+  const [newsSportsImage, setNewsSportsImage]=useState("")
+  const [newsStocksHeader, setNewsStocksHeader]=useState("")
+  const [newsStocksImage, setNewsStocksImage]=useState("")
   useEffect(() => {
       getNews();
+      getMusic();
+      getSports();
   }, [])
   async function getNews(){
-    await Axios.get("https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=demo").then((response)=> {
+    await Axios.get("https://newsdata.io/api/1/latest?apikey=pub_8300799f7287d0011392954caa53af0261b69&q=stocks").then((response)=> {
       try{
-        console.log(response.data.feed)
+        console.log(response)
+        const parsedData = JSON.parse(JSON.stringify(response));
+        setNewsStocksHeader(parsedData.data.results[0].title)
+        setNewsStocksImage(parsedData.data.results[0].image_url)
+        }
+        
+      catch(error){
+        console.error('Error with API:', error);
+      }
        
-        let i =20;
-          setNewsHeader(response.data.feed[i].title);
-          setNewsImage(response.data.feed[i].banner_image);
       
+    })
+  }
+  async function getMusic(){
+    await Axios.get("https://newsdata.io/api/1/latest?apikey=pub_8300799f7287d0011392954caa53af0261b69&q=music").then((response)=> {
+      try{
+        console.log(response)
+        const parsedData = JSON.parse(JSON.stringify(response));
+        setNewsMusicHeader(parsedData.data.results[0].title)
+        setNewsMusicImage(parsedData.data.results[0].image_url)
+        }
+        
+      catch(error){
+        console.error('Error with API:', error);
+      }
+       
+      
+    })
+  }
+  async function getSports(){
+    await Axios.get("https://newsdata.io/api/1/latest?apikey=pub_8300799f7287d0011392954caa53af0261b69&q=sports").then((response)=> {
+      try{
+        console.log(response)
+        const parsedData = JSON.parse(JSON.stringify(response));
+        setNewsSportsHeader(parsedData.data.results[0].title)
+        setNewsSportsImage(parsedData.data.results[0].image_url)
         }
         
       catch(error){
@@ -38,37 +77,36 @@ const Dashboard = () => {
     <>
     
       <Container fluid className={styles.dashboardContainer}>
+        <h3 class="display-1" style={{fontSize: '40px'}}>News of the day</h3>
       <Carousel style={{width: '600px',marginLeft: 'auto', marginRight: 'auto'}}>
-        <Carousel.Item interval={1500}>
+      <Carousel.Item interval={1500}>
           <img
-          src="https://lh6.googleusercontent.com/proxy/NgkNfPvCY8PgW0Rg2WmgVbky9KaATmj8i2eRoq7kQEdgwd7ygtrAXNZMM2JroWOU5gpODQqVBMzxdHaiEpe7ZxKerkqTsCxSdtlcQ54oe_BzEOI9Z0Wg-R7g6m7h2eu6JS5WPMt7rZuspWTVnD4rSvVGIl8"
+          src={newsSportsImage}
             className="d-block w-100"
-            alt="Image One"
+            alt="Image One" style={{width: '200px', height: '400px'}}
           />
           <Carousel.Caption>
-            <h3>Music fact</h3>
-            <p>Details about the fact go here</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-          src="https://lh6.googleusercontent.com/proxy/NgkNfPvCY8PgW0Rg2WmgVbky9KaATmj8i2eRoq7kQEdgwd7ygtrAXNZMM2JroWOU5gpODQqVBMzxdHaiEpe7ZxKerkqTsCxSdtlcQ54oe_BzEOI9Z0Wg-R7g6m7h2eu6JS5WPMt7rZuspWTVnD4rSvVGIl8"
-            className="d-block w-100"
-            alt="Image Two"
-          />
-          <Carousel.Caption>
-            <h3>Sports fact</h3>
-            <p>Details about the fact go here</p>
+            <h3>{newsSportsHeader}</h3>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item interval={1500}>
           <img
-          src={newsImage}
+          src={newsMusicImage}
             className="d-block w-100"
-            alt="Image One"
+            alt="Image One" style={{width: '400px', height: '400px'}}
           />
           <Carousel.Caption>
-            <h3>{newsHeader}</h3>
+            <h3>{newsMusicHeader}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+          src={newsStocksImage}
+            className="d-block w-100"
+            alt="Image One" style={{width: '200px', height: '400px'}}
+          />
+          <Carousel.Caption>
+            <h3>{newsStocksHeader}</h3>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
