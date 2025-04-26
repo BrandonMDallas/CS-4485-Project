1950fd8a8be0ccdd9337842c9b2b74efa6145384
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2025-04-26 16:38:15 -0500

    Fixing the axios problem

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { AuthProvider } from './context/AuthProvider';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AuthProvider } from "./context/AuthProvider";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-        <App />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
